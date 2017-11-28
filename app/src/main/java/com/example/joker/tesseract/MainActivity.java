f1ad7bb75687cc0643f1f201b6d836e66f2b474d
Shriom <shriomtripathi33@gmail.com>
2017-11-28 11:59:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/joker/tesseract/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.joker.tesseract;
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    Bitmap image;
+    private TessBaseAPI mTess;
+    String datapath = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = BitmapFactory.decodeResource(getResources() , R.drawable.phototest);
+        datapath = getFilesDir() + "/tesseract/";
+
+        checkFile(new File(datapath + "tessdata/"));
+        //checkConfig(new File(datapath +"tessdata/configs/"));
+
+        String lang = "eng";
+        mTess = new TessBaseAPI();
+        mTess.init(datapath,lang);
+
+    }
+
+
+
+    //method to copy assests raw data to file (external or internal file of device)
+    private  void copyFiles(){
+
+        try {
+            //location we want the file to be at
+            String filepath = datapath + "/tessdata/eng.traineddata";
+
+            //get access to AssetManager
+            AssetManager assetManager = getAssets();
+
+            //open byte streams for reading/writing
+            InputStream instream = assetManager.open("tessdata/eng.traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            //copy the file to the location specified by filepath
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    //to check wether file exists or not
+    private void checkFile(File dir){
+
+        //directory does not exist, but we can successfully create it
+        if (!dir.exists()&& dir.mkdirs()){
+            copyFiles();
+        }
+        //The directory exists, but there is no data file in it
+        if(dir.exists()) {
+            String datafilepath = datapath+ "/tessdata/eng.traineddata";
+            File datafile = new File(datafilepath);
+            if (!datafile.exists()) {
+                copyFiles();
+            }
+        }
+
+    }
+
+    public void processImage(View view) {
+
+        String OCRresult = null;
+        mTess.setImage(image);
+        mTess.readConfigFile("testfunc");
+        OCRresult = mTess.getUTF8Text();
+        TextView OCRTextView = (TextView) findViewById(R.id.OCRTextView);
+        OCRTextView.setText(OCRresult);
+
+    }
+}
